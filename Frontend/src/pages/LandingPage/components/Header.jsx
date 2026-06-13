c2b625c93f72561e7f7d4f79a7d39aa278b6ceed
Danyal Fayaz <danyalfayaz3102@gmail.com>
2026-06-13 19:43:27 +0500

    sign up change from <a> to buttons

--- Frontend/src/pages/LandingPage/components/Header.jsx
+++ Frontend/src/pages/LandingPage/components/Header.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../context/AuthContext";
 
 const Header = () => {
-  const {user, isAuthenticated} = useAuth(); 
+  const { user, isAuthenticated } = useAuth();
   const navigate = useNavigate();
 
   return (
@@ -54,10 +54,12 @@ const Header = () => {
               <div className="flex items-center space-x-3">
                 <span className="text-gray-700">Welcome, {user?.fullName}</span>
                 <a
-                  href={
-                    user?.role === "employer"
-                      ? "/employer-dashboard"
-                      : "/find-jobs"
+                  onClick={() =>
+                    navigate(
+                      user?.role === "employer"
+                        ? "/employer-dashboard"
+                        : "/find-jobs",
+                    )
                   }
                   className="bg-linear-to-r from-blue-600 to-purple-600 text-white px-6 py-2 rounded-lg font-medium hover:from-blue-700 hover:to-purple-700 transition-all duration-300 shadow-sm hover:shadow-md"
                 >
@@ -65,20 +67,21 @@ const Header = () => {
                 </a>
               </div>
             ) : (
-              <>
-                <a
-                  href="/login"
+              <div>
+                <button
+                  onClick={() => navigate("/login")}
                   className="text-gray-600 hover:text-gray-900 transition-colors font-medium px-4 py-2 rounded-lg hover:bg-gray-100"
                 >
                   login
-                </a>
-                <a
-                  href="/sign-up"
+                </button>
+
+                <button
+                  onClick={() => navigate("/sign-up")}
                   className="bg-linear-to-r from-blue-600 to-purple-600 text-white px-6 py-2 rounded-lg font-medium hover:from-blue-700 hover:to-purple-700 transition-all duration-300 shadow-sm hover:shadow-md"
                 >
                   Sign Up
-                </a>
-              </>
+                </button>
+              </div>
             )}
           </div>
         </div>
